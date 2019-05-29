ec0752ee2b111e3e5d4fa8642ecc44692dfa9796
Jelle Spijker <j.spijker@ultimaker.com>
2019-05-29 11:43:43 +0200

    dill serialization is now possible
    material type factory and reduce function added

--- mechmat/material.py
+++ mechmat/material.py
@@ -8,11 +8,60 @@ from mechmat.core.chainable import Chainable
 __all__ = ['material_factory']
 
 
-class Base(Chainable):
-    def __init__(self, **kwargs):
-        super(Base, self).__init__(**kwargs)
+def material_factory(*args, flow=False, **kwargs):
+    r"""
+    Material instance facotry
 
+    Args:
+        *args: Chainable sub-propperties
+        flow: is the material a continium flowing :math:`\frac{\text{d}m}{}`
+        **kwargs:
+
+    Returns:
+
+    """
+    Material = material_type_factory(*args, flow=flow)
+    return Material()(**kwargs)
+
+
+def material_type_factory(*args, flow=False):
+    if flow:
+        class FlowMaterial(Material, Thermal, Pressure, Flow, *args):
+            def __init__(self, **kwargs):
+                super(FlowMaterial, self).__init__(**kwargs)
+
+        FlowMaterial.dtypes = args
+        FlowMaterial.flow = flow
+
+        return FlowMaterial
+    else:
+        class StaticMaterial(Material, Thermal, Pressure, Geometry, Mass, *args):
+            def __init__(self, **kwargs):
+                super(StaticMaterial, self).__init__(**kwargs)
+
+        StaticMaterial.dtypes = args
+        StaticMaterial.flow = flow
+        return StaticMaterial
+
+
+class _InitializedMaterial(object):
+    def __call__(self, *args):
+        obj = _InitializedMaterial()
+        obj.__class__ = material_type_factory(*args[0], flow=args[1])
+        return obj
+
+
+class Material(Chainable):
+    def __init__(self, **kwargs):
+        super(Material, self).__init__(**kwargs)
         self._logistic_properties += ['name', 'short_name', 'CAS']
+
+    def __reduce__(self):
+        return (_InitializedMaterial(), (self.dtypes, self.flow), self.__dict__)
+
+    dtypes = ()
+
+    flow = False
 
     _version = 1
     """int: version of the material class. Bump this value up for big changes in the class which aren't compatible with 
@@ -43,22 +92,3 @@ class Base(Chainable):
     @short_name.setter
     def short_name(self, value):
         self._short_name = value
-
-
-def material_factory(*args, flow=False, **kwargs):
-    class Material(Base, Thermal, Pressure, *args):
-        def __init__(self, **kwargs):
-            super(Material, self).__init__(**kwargs)
-
-    if flow:
-        class FlowMaterial(Material, Flow):
-            def __init__(self, **kwargs):
-                super(FlowMaterial, self).__init__(**kwargs)
-
-        return FlowMaterial()(**kwargs)
-    else:
-        class StaticMaterial(Material, Geometry, Mass):
-            def __init__(self, **kwargs):
-                super(StaticMaterial, self).__init__(**kwargs)
-
-        return StaticMaterial()(**kwargs)
