d8d77cf94393a987cef0017766fb85321f7b19c1
Jelle Spijker <j.spijker@ultimaker.com>
2019-07-09 08:29:02 +0200

    bug-fix

--- mechmat/__init__.py
+++ mechmat/__init__.py
@@ -9,6 +9,7 @@ __version__ = '0.2.3'
 from pint import UnitRegistry, set_application_registry
 
 ureg = UnitRegistry(autoconvert_offset_to_baseunit=True, default_as_delta=False)
+ureg.setup_matplotlib(True)
 Q_ = ureg.Quantity
 set_application_registry(ureg)
 
--- mechmat/core/bibliography.py
+++ mechmat/core/bibliography.py
@@ -3,4 +3,4 @@ from pkg_resources import resource_filename
 
 bib = Bibliography()
 bib_file = resource_filename('mechmat', 'resources/sources.bib')
-bib.load_bib(bib_file)
+bib.load_bib(bib_file, bib.loaded)
